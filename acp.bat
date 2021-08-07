210d840820dc97c78171b7dc50aa7a8bceb5b361
Ivan Gudelj <gudeljiv@gmail.com>
2021-08-07 13:48:31 +0200

    .

--- acp.bat
+++ acp.bat
@@ -1,4 +1,24 @@
-@Echo off
+@echo off
+setlocal enableDelayedExpansion
+
+
+:: Define the line with variable place holders
+:: Quoted ! literal must be escaped as ^! when delayed expansion is enabled
+set "var=Removing,policy,^!val1^!,from,group,^!val2^!"
+
+:: Show the starting value
+echo var=!var!
+
+:: Define the replacement values
+set "val1=abc"
+set "val2=xyz"
+
+:: Perform the substitution
+for /f "delims=" %%A in ("!var!") do set "var=%%A"
+
+:: Show the result
+echo var=!var!
+
 
 echo Adding files to git: 
 @git add .  >nul 2>&1
