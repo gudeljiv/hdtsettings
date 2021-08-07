bb72af7ba9a44d0ea4a82daa81c5c881a589e288
Ivan Gudelj <gudeljiv@gmail.com>
2021-08-07 13:52:22 +0200

    .

--- acp.bat
+++ acp.bat
@@ -2,14 +2,14 @@
 setlocal enableDelayedExpansion
 
 
-echo Adding files to git: 
-@git add .  >nul 2>&1 && (
-  echo yourCommand was successful
-) || (
-  echo yourCommand failed
-)
-echo --------------
-echo  done
-@git commit -m "."  >nul 2>&1
-@git pull  >nul 2>&1
-@git push  >nul 2>&1
+echo /p Adding files to git: 
+@git add .  >nul 2>&1 && (echo done) || (echo error)
+
+echo /p Commiting files: 
+@git commit -m "."  >nul 2>&1 && (echo done) || (echo error)
+
+echo /p Pulling from git: 
+@git pull  >nul 2>&1 && (echo done) || (echo error)
+
+echo /p Pushing changes to git: 
+@git push  >nul 2>&1 && (echo done) || (echo error)
