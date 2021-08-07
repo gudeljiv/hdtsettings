073c474ca9759fc62fb24970be84758538403010
Ivan Gudelj <gudeljiv@gmail.com>
2021-08-07 13:53:55 +0200

    .

--- acp.bat
+++ acp.bat
@@ -1,6 +1,15 @@
 @echo off
 setlocal enableDelayedExpansion
 
+@echo off
+cls
+echo [101;93m STYLES [0m
+echo ^<ESC^>[0m [0mReset[0m
+echo ^<ESC^>[1m [1mBold[0m
+echo ^<ESC^>[4m [4mUnderline[0m
+echo ^<ESC^>[7m [7mInverse[0m
+echo.
+
 
 echo|set /p= Adding files to git: 
 @git add .  >nul 2>&1 && (echo done [32mGreen[0m) || (echo error)
