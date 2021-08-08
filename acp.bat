f40937abeafb27584748889c8e736f2e8b84e654
Ivan Gudelj <ivan@borming.hr>
2021-08-08 18:20:20 +0200

    .

--- acp.bat
+++ acp.bat
@@ -3,13 +3,13 @@
 setlocal enableDelayedExpansion
 
 echo|set /p= Adding files to git: 
-@git add .  >nul 2>&1 && (echo done) || (echo error)
+@git add . >nul 2>&1 && (echo done) || (echo error)
 
 echo|set /p= Commiting files: 
-@git commit -m "."  >nul 2>&1 && (echo done) || (echo error)
+@git commit -m "." >nul 2>&1 && (echo done) || (echo error)
 
 echo|set /p= Pulling from git: 
-@git pull  >nul 2>&1 && (echo done) || (echo error)
+@git pull >nul 2>&1 && (echo done) || (echo error)
 
 echo|set /p= Pushing changes to git: 
-@git push  >nul 2>&1 && (echo done) || (echo error)
+@git push >nul 2>&1 && (echo done) || (echo error)
